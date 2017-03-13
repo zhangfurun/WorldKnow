3157394c918fde6482425b1a5514180133cab475
zhangfurun <122674287@qq.com>
2017-03-13 20:31:46 +0800

    1

---
+++ WorldKnow/WorldKnow/TitleTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  TitleTableViewCell.h
+//  WorldKnow
+//
+//  Created by 张福润 on 16/2/29.
+//  Copyright © 2016年 张福润. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TitleTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *labelTitle;
+@property (weak, nonatomic) IBOutlet UILabel *labelCome;
+@property (weak, nonatomic) IBOutlet UILabel *labelTime;
+
+@end
---
+++ WorldKnow/WorldKnow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WorldKnow
+//
+//  Created by 张福润 on 16/2/2.
+//  Copyright © 2016年 张福润. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
