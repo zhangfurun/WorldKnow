79c4b60a18815dc8caa348a0716ab520b7306377
zhangfurun <122674287@qq.com>
2017-03-13 16:51:14 +0800

    1.上传工程

---
+++ WorldKnow/WorldKnow/AllListName.h
@@ -0,0 +1,15 @@
+//
+//  AllListName.h
+//  WorldKnow
+//
+//  Created by 张福润 on 16/3/2.
+//  Copyright © 2016年 张福润. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AllListName : NSObject
++(instancetype)shareAllList;
+
+-(NSDictionary *)getAllList;
+@end
---
+++ WorldKnow/WorldKnow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorldKnow
+//
+//  Created by 张福润 on 16/2/2.
+//  Copyright © 2016年 张福润. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVOSCloud/AVOSCloud.h>
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
