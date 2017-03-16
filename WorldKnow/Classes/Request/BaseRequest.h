b9949bf7d2ee3e5361be12917af917d5c0ce6c8a
zhangfurun <ifenghui@zhangfurundeiMac.local>
2017-03-16 11:12:12 +0800

    1.添加Request占位文件

---
+++ WorldKnow/Classes/Request/BaseRequest.h
@@ -0,0 +1,13 @@
+//
+//  BaseRequest.h
+//  WorldKnow
+//
+//  Created by 张福润 on 2017/3/16.
+//  Copyright © 2017年 张福润. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseRequest : NSObject
+
+@end
---
+++ WorldKnow/Classes/Request/BaseRequest.m
@@ -0,0 +1,13 @@
+//
+//  BaseRequest.m
+//  WorldKnow
+//
+//  Created by 张福润 on 2017/3/16.
+//  Copyright © 2017年 张福润. All rights reserved.
+//
+
+#import "BaseRequest.h"
+
+@implementation BaseRequest
+
+@end
